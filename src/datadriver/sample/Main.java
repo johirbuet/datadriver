d79b3ab54bd4e7db34b0a580f42c5e8383cd2394
Md Johirul Islam <johirbuet@gmail.com>
2016-11-08 08:59:22 -0600

    Protocol

--- src/datadriver/sample/Main.java
+++ src/datadriver/sample/Main.java
@@ -9,14 +9,14 @@ import datadriver.mongo.protoToMongooseSchema;
 public class Main {
 	public static void main(String[] args) {
 		ReadDBF rdb=new ReadDBF();
-		String link="/Users/mislam/Desktop/Traffic Shared Data/FARS/2015/FARS2015NationalDBF/accident.dbf";
+		String link="/Users/mislam/Desktop/Traffic Shared Data/FARS/2015/FARS2015NationalDBF/vehicle.dbf";
 		rdb.readFromLocal(link);
 		rdb.printSchema();
 		rdb.writeToProto("boa.transportation.types");
-		rdb.writeMongSchema();
-		ArrayList<String> names=rdb.getNames();
-		WriteToMongo wm=new WriteToMongo();
-		wm.writeValues(names, "fars2015", link,"accidents");
+		//rdb.writeMongSchema();
+		//ArrayList<String> names=rdb.getNames();
+		//WriteToMongo wm=new WriteToMongo();
+		//wm.writeValues(names, "fars2015", link,"accidents");
 		//protoToMongooseSchema pm=new protoToMongooseSchema("Accident.proto");
 		//System.out.println(pm.getMongooseSchema());
 		///pm.writeMongooseSchema();
